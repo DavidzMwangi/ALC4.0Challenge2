2c8899814361b736bb39b8c06431435ea96715d3
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-08-05 12:30:13 +0300

    completed code on the authentication

---
+++ app/src/main/java/ke/co/davidwanjohi/travelmantics/FirebaseUtil.java
@@ -0,0 +1,26 @@
+package ke.co.davidwanjohi.travelmantics;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class FirebaseUtil {
+
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth firebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+//    public static ArrayList<TravelDeal> mDeals;
+
+
+    private FirebaseUtil(){
+
+
+    }
+    public static void openFbReference(String ref){
+
+    }
+}
